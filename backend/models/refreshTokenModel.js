408049ac8be01054233679653c837ecac352e5fd
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-21 10:52:55 +0100

    inserting/deleting token

--- backend/models/refreshTokenModel.js
+++ backend/models/refreshTokenModel.js
@@ -1,3 +1,31 @@
 import { PrismaClient } from "@prisma/client";
+import { safeQuery } from "../utility/queryWrapper.js";
 
 const prisma = new PrismaClient();
+
+// insert new token
+async function insertToken(token) {
+    await safeQuery(() => 
+        prisma.refreshToken.create({
+            data: {
+                token: token
+            }
+        })
+    );
+}
+
+// delete token
+async function deleteToken(token) {
+    await safeQuery(() => 
+        prisma.refreshToken.delete({
+            where: {
+                token: token
+            }
+        })
+    );
+}
+
+export default {
+    insertToken,
+    deleteToken
+}
