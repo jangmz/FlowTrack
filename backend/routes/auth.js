b271d1788c2641b701f7579c5b36e54a3277583c
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-22 13:43:10 +0100

    validation middleware added for user sign up

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import authController from "../controllers/auth.js";
+import signUpValidation from "../validation/userValidation.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", authController.signUp);
+authRouter.post("/sign-up", signUpValidation, authController.signUp);
 authRouter.post("/log-in", authController.logIn);
 authRouter.delete("/log-out", authController.logOut);
 authRouter.post("/token", authController.refreshToken);
