4d9e70dedf3473706b5457e1e587043567b4ceca
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-24 11:06:09 +0100

    protected routes with admin privileges

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import userController from "../controllers/user.js";
+import { adminAuthorization } from "../middleware/authCheck.js";
 
 const userRouter = Router();
 
-userRouter.get("/", userController.getUsers);
-userRouter.post("/", userController.insertUser);
-userRouter.delete("/:userId", userController.deleteUser);
-userRouter.put("/:userId", userController.updateUser);
+userRouter.get("/", adminAuthorization, userController.getUsers);
+userRouter.post("/", adminAuthorization, userController.insertUser);
+userRouter.delete("/:userId", adminAuthorization, userController.deleteUser);
+userRouter.put("/:userId", adminAuthorization, userController.updateUser);
 
 export default userRouter;
