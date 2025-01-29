7348628451b5ef5fd4b8fbf96ec7bfe3b91f3b37
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-29 14:11:40 +0100

    log in form

--- frontend/src/components/general/LogIn.jsx
+++ frontend/src/components/general/LogIn.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+
 export default function LogIn() {
+    const [logInUser, setlogInUser] = useState({
+        username: "",
+        password: ""
+    });
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setlogInUser({ ...logInUser, [name]: value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(logInUser);
+        // TODO: call AuthContext for user authentication
+    }
+
     return (
-        <>
-            Log in form
-        </>
+        <div className="container d-flex flex-column align-items-center">
+            <h1>Log In</h1>
+            <form className="d-flex flex-column w-50" onSubmit={(e) => handleSubmit(e)}>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Username</label>
+                    <input type="text" name="username" id="username" className="form-control" required onChange={(e) => handleChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" name="password" id="password" className="form-control" required onChange={(e) => handleChange(e)}/>
+                </div>
+                <button type="submit" className="btn btn-primary" >Log In</button>
+            </form>
+        </div>
     )
 }
