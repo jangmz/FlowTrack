1e174f2e2a944cba54d5f521eff0267ffe46ce61
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-02-01 18:50:42 +0100

    changed layout of the navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -10,14 +10,14 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">
+                <a className="navbar-brand d-flex align-items-center gap-2" href="/">
                     <img src={Logo} alt="FlowTrack logo" width="30" height="auto"/>
                     <span className="text-white fw-bold">FlowTrack</span>
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     {
                         user 
                         ? <AdminMenuLinks />
