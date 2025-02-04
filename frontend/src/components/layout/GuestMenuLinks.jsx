37bf01f0f51965db48377eebde9e751c2e32df48
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-02-04 12:02:44 +0100

    links on the right side

--- frontend/src/components/layout/GuestMenuLinks.jsx
+++ frontend/src/components/layout/GuestMenuLinks.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function GuestMenuLinks() {
     return (
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item">
                 <Link className="nav-link text-white" to="/sign-up">Sign Up</Link>
             </li>
