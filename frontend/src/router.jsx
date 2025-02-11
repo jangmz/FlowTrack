32bca4ea05c67cb497442319956a0d070bb77da0
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-02-11 13:34:48 +0100

    added new path for editing devices

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,21 +1,23 @@
 import Home from "./pages/Home";
 import SignUp from "./components/general/SignUp";
 import LogIn from "./components/general/LogIn";
-import Dashboard from "./components/inventory/Dashboard";
 import Laptops from "./components/inventory/Laptops";
 import Tablets from "./components/inventory/Tablets";
 import Desktops from "./components/inventory/Desktops";
 import Projectors from "./components/inventory/Projectors";
+import NewDeviceForm from "./components/inventory/NewDeviceForm";
+import EditDeviceForm from "./components/inventory/EditDeviceForm";
 
 const routes = [
     { index: true, element: <Home /> }, // renders at "/"
     { path: "sign-up", element: <SignUp /> },
     { path: "log-in", element: <LogIn /> },
-    { path: "dashboard", element: <Dashboard /> },
     { path: "devices/laptops", element: <Laptops /> },
     { path: "devices/tablets", element: <Tablets /> },
     { path: "devices/desktops", element: <Desktops /> },
     { path: "devices/projectors", element: <Projectors /> },
+    { path: "devices/new", element: <NewDeviceForm /> },
+    { path: "devices/edit/:deviceId", element: <EditDeviceForm /> },
 ]
 
 export default routes;
