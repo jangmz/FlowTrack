a4d0563b2340695216d405d1a602c382338a09a4
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-02-11 13:40:10 +0100

    moved deviceTypes values

--- frontend/src/components/general/DropdownSelection.jsx
+++ frontend/src/components/general/DropdownSelection.jsx
@@ -1,10 +1,17 @@
-export default function DropdownSelection({ label, name, selectValue, values, onChange }) {
+export default function DropdownSelection({ label, name, selectValue, onChange }) {
+    const deviceTypes = [
+        "Laptop",
+        "Desktop",
+        "Tablet",
+        "Projector"
+    ];
+    
     return (
         <div className="mb-3">
             <label htmlFor={name} className="form-label">{label}</label>
             <select name={name} id={name} value={selectValue} onChange={onChange} className="form-select">
                 {
-                    values.map((value) => (
+                    deviceTypes.map((value) => (
                         <option key={value} value={value}>{value}</option>
                     ))
                 }
