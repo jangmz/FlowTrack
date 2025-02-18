e1f96573aa67871363954560148b9177d9321bd1
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-02-18 10:36:46 +0100

    removed array

--- frontend/src/components/general/DropdownSelection.jsx
+++ frontend/src/components/general/DropdownSelection.jsx
@@ -1,17 +1,10 @@
-export default function DropdownSelection({ label, name, selectValue, onChange }) {
-    const deviceTypes = [
-        "Laptop",
-        "Desktop",
-        "Tablet",
-        "Projector"
-    ];
-    
+export default function DropdownSelection({ label, name, selectValue, optionValues, onChange }) {       
     return (
         <div className="mb-3">
             <label htmlFor={name} className="form-label">{label}</label>
             <select name={name} id={name} value={selectValue} onChange={onChange} className="form-select">
                 {
-                    deviceTypes.map((value) => (
+                    optionValues.map((value) => (
                         <option key={value} value={value}>{value}</option>
                     ))
                 }
